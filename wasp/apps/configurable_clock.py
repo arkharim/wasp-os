b837d8a220e523b3ba351886950a64d387bad883
arkharim <72758421+arkharim@users.noreply.github.com>
2020-10-12 20:22:36 +0200

    Configurable clock: clean comment code

--- wasp/apps/configurable_clock.py
+++ wasp/apps/configurable_clock.py
@@ -204,25 +204,8 @@ class ConfigurableClockApp():
         self._draw_bezel(120, 120, 115, 120, 2, 5, bz_5)  # 5 minutes marks
         self._draw_bezel(120, 120, 100, 110, 6, 5, bz_15)  # 15 minutes marks
 
-        # angle = 360/dial_number * time - 90
-        # cos = math.cos(math.radians(-angle))
-        # sin = math.sin(math.radians(-angle))
-        #
-        #
-        # # Erase the old hand
-        # if self._bg_image:
-        #     #TODO tmb 1rle
-        #     draw._redraw_rle2bit(_bg_image, on_screen_hand)
-        # # else:
-        # #     for i in range(len(on_screen_hand)):
-        # #         #draw.fill(color, px[1], px[0], 1, 1)
-        # #         draw.fill(color, on_screen_hand[i][1], on_screen_hand[i][0], 1, 1)
-        #
-        # # Paint the new hand
+        # Reset the on screen hand
         for i in range(len(_hour_hand)):
-            #draw.fill(color, on_screen_hand[i][1], on_screen_hand[i][0], 1, 1)
-            # on_screen_hand[i] = [int(y_center + int(hand[i][1] * cos - hand[i][0] * sin)),
-            #                      int(x_center + int(hand[i][0] * cos + hand[i][1] * sin))]
             self._old_hand_hours[i] = [0,0]
 
         for i in range(len(_hour_hand)):
@@ -233,7 +216,6 @@ class ConfigurableClockApp():
 
     def _draw_digital_clock(self):
         draw = wasp.watch.drawable
-        #TODO configurable color
         draw.blit(digits.clock_colon, 2 * 48, 80, fg=dclock_color)  # 48 it's the width of the colons.
 
     def _draw_bezel(self, x_center, y_center, inner_radius, out_radius, width, time_label=1, color=0xffff):
@@ -279,36 +261,6 @@ class ConfigurableClockApp():
         self._on_screen = now
 
     def _update_analog_clock(self, now):
-        # Wipe the old _hand.
-        #TODO hackeado que busca la imagen siempre.
-        #draw = wasp.watch.drawable
-        # TODO soportar imagenes rle1bit.
-        #color = draw.get_blit_color(icons.pine64_rainbow, 165, 40)
-        #color = draw.get_blit_color(icons.pine64_rainbow, 120, 120)
-        #color = draw.get_blit_color(icons.pine64_rainbow, 101, 159)
-
-        #color = draw.get_blit_color(icons.app, 73, 55)
-        #color = draw.get_blit_color(icons.app, 35, 29)
-
-        #color = draw.get_blit_color(icons.pine64_logo, 165, 40)
-
-        #color = draw.get_blit_color(_bg_image, 165, 40)
-
-
-        #for x in range(140,180):
-        #    draw.fill(color, x, 100, 1, 1)
-
-        #draw = wasp.watch.drawable
-        #draw.blit(icons.pine64_logo,0,0,fg=0xffff)
-        #draw.blit(icons.pine64_rainbow, 0, 0)
-        #draw.blit(_bg_image, 0, 0)
-        #draw.blit(icons.app, 120, 120)
-        #Wipe the old _hand
-        # if self._on_screen != -1:
-        #     self._draw_hand(self._on_screen[5], 60, 120, 120, _hand)
-        #     self._draw_hand(self._on_screen[4], 60, 120, 120, _hand)
-        #     self._draw_hand(self._on_screen[3] + self._on_screen[4] / 60, 12, 120, 120, _hand)
-
         # Draw the new _hand: Hours, Minutes, Seconds
         #TODO need to only update to be lazy. The code below don't work because the hnads are no correctly updated.
 
@@ -323,41 +275,8 @@ class ConfigurableClockApp():
         if now[4] != self._on_screen[4] or self._on_screen[5] == self._on_screen[4]:
             self._update_hand(now[4], 60, 120, 120, _minute_hand, self._old_hand_minutes, _minuter_hand_shape[0])
 
-    #TODO borrar
-    def _update_analog_clock_slow(self, now):
-        #TODO es demasiado lento. no es capaz de borrar los 3 a la vez. se nota. Aunque sea solo la maneta de los segundos. Se podria mejorar un poco pero al final se veria en un cierto moento.
-
-        # Wipe the old _hand.
-        #TODO hackeado que busca la imagen siempre.
-        #draw = wasp.watch.drawable
-        # TODO soportar imagenes rle1bit.
-        #color = draw.get_blit_color(icons.pine64_rainbow, 165, 40)
-        #color = draw.get_blit_color(icons.pine64_rainbow, 120, 120)
-        #color = draw.get_blit_color(icons.pine64_rainbow, 101, 159)
-
-        #color = draw.get_blit_color(icons.app, 73, 55)
-        #color = draw.get_blit_color(icons.app, 35, 29)
-
-        #color = draw.get_blit_color(icons.pine64_logo, 165, 40)
-
-        #color = draw.get_blit_color(_bg_image, 165, 40)
-
-
-        #for x in range(140,180):
-        #    draw.fill(color, x, 100, 1, 1)
-
-        #Wipe the old _hand
-        if self._on_screen != -1:
-            self._draw_hand_slow(self._on_screen[5], 60, 120, 120, _hand)
-            self._draw_hand_slow(self._on_screen[4], 60, 120, 120, _hand)
-            self._draw_hand_slow(self._on_screen[3] + self._on_screen[4] / 60, 12, 120, 120, _hand)
-
-        # Draw the new _hand
-        self._draw_hand_slow(now[5], 60, 120, 120, _hand, 0x27E0) #Seconds
-        self._draw_hand_slow(now[4], 60, 120, 120, _hand, 0xffff) #Minutes
-        self._draw_hand_slow(now[3] + now[4] / 60, 12, 120, 120, _hand, 0xF800) #Hours
-
     def _update_digital_clock(self, now):
+        # TODO Add support for background image
         # The digital clock don't show seconds. If the hours and minutes didn't change, only update the widgets.
         if now[3] != self._on_screen[3] or now[4] != self._on_screen[4]:
             # In case that time changes, updated the clock
@@ -382,108 +301,14 @@ class ConfigurableClockApp():
 
         # Erase the old hand
         if self._bg_image:
-            #TODO tmb 1rle
+            #TODO Add suport for 1rle images
             draw._redraw_rle2bit(_bg_image, on_screen_hand)
         else:
             for i in range(len(on_screen_hand)):
-                #draw.fill(color, px[1], px[0], 1, 1)
                 draw.fill(_bg, on_screen_hand[i][1], on_screen_hand[i][0], 1, 1)
 
         # Paint the new hand
         for i in range(len(hand)):
-            #draw.fill(color, on_screen_hand[i][1], on_screen_hand[i][0], 1, 1)
             on_screen_hand[i] = [int(y_center + int(hand[i][1] * cos - hand[i][0] * sin)),
                                  int(x_center + int(hand[i][0] * cos + hand[i][1] * sin))]
             draw.fill(color, on_screen_hand[i][1], on_screen_hand[i][0], 1, 1)
-
-
-
-    #TODO borrar
-    def _draw_hand_slow(self, time, dial_number, x_center, y_center, hand, color=None):
-        #TODO si no hago un fill, solamente se llena la parte que pintas nueva. la antigua now -> "soportamos background
-        draw = wasp.watch.drawable
-        angle = 360/dial_number * time - 90
-        cos = math.cos(math.radians(-angle))
-        sin = math.sin(math.radians(-angle))
-        bg_color = color
-        for pix in hand:
-            u = int(pix[0] * cos + pix[1] * sin)
-            v = int(pix[1] * cos - pix[0] * sin)
-            if color is None:
-                #TODO sopopratar imganes con translacion. que no son 240x240 px.
-                bg_color = draw.get_blit_color(_bg_image, int(x_center + u), int(y_center + v))
-                draw.fill(bg_color, int(x_center + u), int(y_center + v), 1, 1)
-            else:
-                draw.fill(color, int(x_center + u), int(y_center + v), 1, 1)
-
-
-    # def _draw_hand(self, time, dial_number, x_center, y_center, _hand, color=0xffff):
-    #     draw = wasp.watch.drawable
-    #     angle = 360/dial_number * time - 90
-    #     cos = math.cos(math.radians(angle))
-    #     sin = math.sin(math.radians(angle))
-    #     for pix in range(_hand[0], _hand[1], _hand[2]):
-    #         w2 = _hand[3]//2
-    #         draw.fill(color, int(x_center + cos * pix - w2), int(y_center + sin * pix - w2), _hand[3], _hand[3])
-    #TODO borrar
-    def _draw_hand2(self, time, dial_number, x_center, y_center, hand, color=0xffff):
-        angle = 360 / dial_number * time - 90
-
-        #TODO solo in tipo. axctulizar para mas longitudes.
-        for pix in range(hand[0], hand[1], hand[2]):
-            buf = [[x for x in range(hand[0], hand[1], hand[2])] for x in [x for x in range(hand[0], hand[1], hand[2])]]
-            w2 = hand[3] // 2 #TODO espesor siempre impar.
-            for line in buf:
-                for pix in line:
-                    draw.fill(color, int(x_center + cos * pix - w2), int(y_center + sin * pix - w2), hand[3], hand[3])
-
-    #TODO borrar
-    def _rotate(self, pixel, angle, color):
-        """Image = vector [(x1,y1),....]
-
-        http://pippin.gimp.org/image_processing/chap_resampling.html
-
-        """
-        draw = wasp.watch.drawable
-        for y in range(img[1]):
-            for x in range(img[0]):
-                u = x * math.cos(-angle) + y * math.sin(-angle)
-                v = y * math.cos(-angle) - x * math.sin(-angle)
-                #TODO buscar pintaR DIRECTAMENTE PIXEL A PIXEL
-                draw.fill(color, u, v, 1, 1)
-
-    #TODO borrar
-    def hand(self, m, t, cy, c, l, w, r, s, prev):
-        d = wasp.watch.drawable
-        if cy == 0:
-            an = 30 * (t[3] + t[4] / 60)
-        else:  # for 1 and 2
-            an = 6 * t[3 + cy]
-        o = math.cos(math.radians(an))
-        i = math.sin(math.radians(an))
-        ue = int(s[0] / 100 * l)
-        if prev == True:
-            try:
-                if cy == 0:
-                    anold = 30 * (self.ons[3] + self.ons[4] / 60)
-                else:  # for 1 and 2
-                    anold = 6 * self.ons[3 + cy]
-                oold = math.cos(math.radians(anold))
-                iold = math.sin(math.radians(anold))
-            except:
-                pass
-        oc = c
-        for u in s[1:]:
-            c = oc
-            for p in range(int(-l * (u[0] - ue) / 100), int(-l * (u[1] - ue) / 100), -r):
-                try:
-                    c = u[3]
-                    w = u[4]
-                except:
-                    pass
-                of = int(u[2] * l / 100)  # normalize offset to length
-                if prev:
-                    d.fill(bgc, int(m[0] + of * oold - p * iold), int(m[1] + of * iold + p * oold), int(w), int(w))
-                    d.fill(bgc, int(m[0] - of * oold - p * iold), int(m[1] - of * iold + p * oold), int(w), int(w))
-                d.fill(c, int(m[0] + of * o - p * i), int(m[1] + of * i + p * o), int(w), int(w))
-                d.fill(c, int(m[0] - of * o - p * i), int(m[1] - of * i + p * o), int(w), int(w))
